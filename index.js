ee2e2dfeb6bb07986cba480d62941c91fc893a8e
gditrani269 <gditrani269@gmail.com>
2022-01-08 19:51:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
 const express = require("express");
 const app = express();
+const { Pool, Client } = require('pg')
+
+const client = new Client({
+  user: 'humlprjlvdbdnt',
+  host: 'ec2-23-20-124-77.compute-1.amazonaws.com',
+  database: 'ddsogo2cmtup33',
+  password: 'f8280465b2f538726d073816c7862c981b13ada9fbd7a0b6813e7ffe620077a6',
+  port: 5432,
+  //ssl: true
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+client.connect()
+var donde = 'mi donde ger'
+
+app.get('/db', function (req, res) {
+	var oRta = ""
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	client.query("SELECT id, name FROM leads WHERE id = 2;", (err, res) => {
+      oRta = res.rows[0]["id"]
+      console.log(err, oRta, donde)
+	  res.send(oRta);
+//  client.end()
+   })
+	res.send(oRta);
+});
+
+/* GET users listing. */
+app.get('/db2', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+  client.query('SELECT id, estilo, receta FROM cerveza;', (err, result) => {
+   return res.json(result.rows)
+  });
+
+});
+
 
 app.get('/hola', function (req, res) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -14,7 +58,7 @@ app.get('/hola2', function (req, res) {
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.send('[{"estilo": "Irish Red"},{"estilo": "Apa"},{"estilo": "Ipa"},{"estilo": "Porter"},{"estilo": "Stout"},{"estilo": "Honey"}]');
+  res.send('[{"estilo": "Irish Red"},{"estilo": "Apa"},{"estilo": "Ipa"},{"estilo": "Porter"},{"estilo": "Stout"}]');
 });
 
 
@@ -36,9 +80,6 @@ app.get('/recetas/:nombre', function(req, res) {
 	if (req.params.nombre === "Stout" ) {
 		sMens = "Stout"
 	}
-	if (req.params.nombre === "Honey" ) {
-		sMens = "Honey"
-	}
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -47,6 +88,27 @@ app.get('/recetas/:nombre', function(req, res) {
   
 });
 
+app.get('/recetas2/:nombre', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    console.log(req.params.nombre);
+	var sMens = req.params.nombre
+    console.log(sMens);
+	var que = {
+		  // give the query a unique name
+		  name: 'fetch-user',
+		  text: "SELECT receta FROM cerveza WHERE estilo = $1",
+		  values: [sMens],
+		}
+//  client.query("SELECT receta FROM cerveza WHERE estilo = ${sMens};", (err, result) => {
+  client.query(que, (err, result) => {
+	  console.log(err, result);
+   return res.json(result.rows)
+  });
+});  
+  
 app.get('/:id', function (req, res) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
